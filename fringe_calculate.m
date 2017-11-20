c277cfbea4dad1dbd70581a746e1c083358e953f
chenxiaojie92 <1548048291@qq.com>
2017-11-20 23:07:42 +0800

    calculate.m to get the data on the detected plane, is the combination of main_small(big)_magnetic_field,plot.m is used to plot the fringe with sparse data, large amount data need too much time ,this way can save some time to see the overview of the fringe

---
+++ fringe_calculate.m
@@ -0,0 +1,305 @@
+%% this program contain three parts
+% 1 Get the classical motion and classical varibles
+% 2 Get the fringes on the detached plane
+% 3 Fit the fringes and extrapolate the data outsides the classical
+% boundary
+%Notice
+%This program has been modified to apply to p1-wave and big magnetic field
+%and small magnetic field at the same time.
+%% First part Classical motion and varibles
+syms F B A m e t omega z0 E R 
+syms x(t) y(t) z(t) 
+syms p_x(t) p_y(t) p_z(t) 
+velocity = sqrt(2*m*E)        ;
+H(t) =p_z(t)^2/(2*m) +1/2*m*omega^2*(z(t)+p_x(t)/(m*omega))^2 +p_y(t)^2/(2*m)+F*e*z(t);%Hamitonian
+syms theta phi
+% Hamitonian Equations
+equl(1) = diff(x(t),t)   == functionalDerivative(H(t), p_x(t));
+equl(2) = diff(y(t),t)   == functionalDerivative(H(t), p_y(t));
+equl(3) = diff(z(t),t)   == functionalDerivative(H(t),p_z(t));
+equl(4) = diff(p_x(t),t) == -functionalDerivative(H(t), x(t));
+equl(5) = diff(p_y(t),t) == -functionalDerivative(H(t), y(t));
+equl(6) = diff(p_z(t),t) == -functionalDerivative(H(t),z(t));
+vx(t) = diff(x(t), t);
+vy(t) = diff(y(t), t);
+vz(t) = diff(z(t), t);
+% Initial Conditions
+cond(1) = x(0) ==  R*sin(theta)*cos(phi);
+cond(2) = y(0) ==  R*sin(theta)*sin(phi);
+cond(3) = z(0) ==  z0+R*cos(theta);
+cond(4) = vx(0) == velocity*sin(theta)*cos(phi);
+cond(5) = vy(0) == velocity*sin(theta)*sin(phi);
+cond(6) = vz(0) == velocity*cos(theta);
+% Solution: and get the expressions of coordinate velocity and momentum
+Bs = dsolve(equl(:), cond(:));
+xSol(t) = Bs.x;
+ySol(t) = Bs.y;
+zSol(t) = Bs.z;
+vx(t) = diff(xSol,t);
+vy(t) = diff(ySol,t);
+vz(t) = diff(zSol,t);
+p_x = Bs.p_x ;
+p_y = Bs.p_y ;
+p_z = Bs.p_z ;
+% Get the time reaching the detecting plane
+tmax = solve(zSol(t) ==0, t);% two solution ,second result above zero can be used
+
+fun_s = p_x*real(vx) + p_y*vy + real(p_z)*real(vz);%-e*zSol*vx*B;
+S = int( fun_s ,t, 0,real(tmax(2)));
+% display(S);
+xSol_sin=rewrite(xSol,'sincos');
+xSol_real= simplify(xSol_sin,'Criterion','preferReal');
+zSol_sin=rewrite(zSol,'sincos');
+zSol_real= simplify(zSol_sin,'Criterion','preferReal');
+vx_real = simplify(rewrite(vx,'sincos'),'Criterion','preferReal');
+p_z_real = simplify(rewrite(p_z,'sincos'),'Criterion','preferReal');
+vz_real = simplify(rewrite(vz,'sincos'),'Criterion','preferReal');
+Jt= det(jacobian([xSol_real ySol zSol_real],[t theta phi]));
+% display(Jt);
+%% Setting the parameter
+% We will often change the value of magnetic field strength
+E = 8.352*10^-5/27.211385     ;  %a.u. 83.52 uev
+F = 2.91*10^2/(5.142206*10^11);  %a.u. 291 v/m
+B = 81*10^-6/(2.35*10^5)    ;  %a.u. 1.1 uT
+L_au =  5.2917721092*10^-11;     %(m)
+R = 0                        ;  %(a0) as radius of sphere
+z0 = 0.5 / L_au               ;  %a.u. 0.5 m
+e= 1;
+m =1;
+omega = B*e/m                 ;  %period
+xSol_num = matlabFunction(subs(xSol_real));%(t, phi, theta)
+ySol_num = matlabFunction(subs(ySol));%(t, phi, theta)
+zSol_num = matlabFunction(subs(zSol));
+tmax_sym = subs(tmax(2));
+tmax_num = matlabFunction(tmax_sym);%(phi,theta)
+xDetect = matlabFunction(subs(xSol(tmax(2))));%(phi,theta)
+yDetect = matlabFunction(subs(ySol(tmax(2)))); %this number is in S.I.
+action =matlabFunction(subs(S));%(phi, theta)
+R =100;
+Jacob = matlabFunction(subs(Jt));%%(t phi theta)
+%% second part Fringes on the detated plane
+T_cpu_1 = cputime;
+num_phi = 500; %500
+num_theta   = 200; %200
+num_point = num_phi*num_theta;
+initial_angle = zeros(num_point,2);%[theta phi]
+Matrix_a = linspace(0,pi,num_phi);
+Matrix_b = [];
+ma = linspace(pi/2000,pi/2*930/1000,num_theta); %%set theta range pi/20000, 
+% ma = linspace(pi/2*800/1000,pi/2*970/1000,num_theta);
+mb = [];
+for j = 1:num_theta
+    Matrix_b = [Matrix_b Matrix_a];
+    mb = [mb ma(j)*ones(1,num_phi)];
+end
+initial_angle(:,2)= Matrix_b';
+initial_angle(:,1)= mb';
+theta_2 = zeros(1,num_point);
+phi_2   = zeros(1,num_point);
+xmn     = zeros(1,num_point);
+ymn     = zeros(1,num_point);
+amp1    = zeros(1,num_point); %phi <1/2*pi,trajectory amptitude
+amp2    = zeros(1,num_point); %phi >1/2*pi
+amp_airya= zeros(1,num_point); %airy approximation
+s1      = zeros(1,num_point);
+s2      = zeros(1,num_point);
+delta_s = zeros(1,num_point);
+delta_a = zeros(1,num_point);
+flag    = zeros(1,num_point);
+sum_s   = zeros(1,num_point);
+gamma   = zeros(1,num_point);
+delta   = zeros(1,num_point);
+zeta    = zeros(1,num_point);
+amp_m   = zeros(1,num_point);
+amp_p   = zeros(1,num_point);
+
+options = optimoptions('fsolve','Display','none');
+% options.FunctionTolerance=10^-10;
+% options.OptimalityTolerance = 10^-10 ;
+% options.StepTolerance = 10^-10;
+for i = linspace(1,num_point,num_point)
+    tx = real(tmax_num(initial_angle(i,2),initial_angle(i,1)));
+    xmn(i) = xSol_num(tx,initial_angle(i,2),initial_angle(i,1));
+    ymn(i) = ySol_num(tx,initial_angle(i,2),initial_angle(i,1));
+    amp1(i) = abs(Jacob(0,initial_angle(i,2),initial_angle(i,1))/Jacob(tx,initial_angle(i,2),initial_angle(i,1)))^(1/2);
+    s1(i) = action(initial_angle(i,2), initial_angle(i,1));
+    fun1 = @(ang)[real(xDetect(ang(1), ang(2))-xmn(i)) ;
+        real(yDetect(ang(1), ang(2))-ymn(i))];
+    x0 = [initial_angle(i,2), pi-initial_angle(i,1)];
+    [mm1,MML,flag(i),MMM] = fsolve(fun1,x0, options);%%mm1 [phi theta]
+    phi_2(i) = mm1(1);
+    theta_2(i) = mm1(2);
+    T2 = real(tmax_num(phi_2(i), theta_2(i)));
+    amp2(i)=abs(Jacob(0,phi_2(i),theta_2(i))/Jacob(T2,phi_2(i),theta_2(i)))^(1/2);
+    s2(i) = action(phi_2(i), theta_2(i));
+    delta_s(i) = s1(i)-s2(i);
+    sum_s(i) = s2(i)+s1(i);
+    zeta(i) = (3/4*delta_s(i))^(2/3);
+% % %     s-wave
+%     amp_m(i) = amp2(i) -amp1(i);
+%     amp_p(i) = amp2(i) +amp1(i);
+% %   p0-wave
+%     amp_m(i) = amp2(i)*cos(theta_2(i)) - amp1(i)*cos(initial_angle(i,1));
+%     amp_p(i) = amp2(i)*cos(theta_2(i)) + amp1(i)*cos(initial_angle(i,1));
+% %     p1-wave
+    amp_m(i) = amp2(i)*sin(theta_2(i))*exp(-1i*phi_2(i)) -amp1(i)*sin(initial_angle(i,1))*exp(-1i*initial_angle(i,2));
+    amp_p(i) = amp2(i)*sin(theta_2(i))*exp(-1i*phi_2(i)) +amp1(i)*sin(initial_angle(i,1))*exp(-1i*initial_angle(i,2));
+    gamma(i) = sqrt(pi)*exp(1i*sum_s(i))*amp_p(i)*zeta(i)^(1/4);
+    delta(i) = sqrt(pi)*exp(1i*sum_s(i))*amp_m(i)*zeta(i)^(-1/4);
+    amp_airya(i) = abs(gamma(i)*airy(-zeta(i))-1i*delta(i)*airy(1,-zeta(i)))^2;
+end
+%% third part fitting part the fitting area is chosen by the determinating the number of the initial_theta.
+% connect the phi_pattern and phi_sys
+x_boun =[];
+y_boun =[];
+for i = linspace(0,pi/2,101) % num_of_theta 100
+    for j = linspace(0,pi,101) % num_of_phi 100
+        t_boun = real(tmax_num(j , i));
+        x_boun = [x_boun ;xSol_num(t_boun,j,i)*L_au i j];
+        y_boun = [y_boun ;ySol_num(t_boun,j,i)*L_au i j];
+    end
+end
+phi_pattern =[];
+for i_phi = linspace(0,pi,101)
+    num_boun = find(x_boun(:,3) ==i_phi);
+    phi_pat = atan(y_boun(num_boun(end),1)/(x_boun(num_boun(end),1)-x_boun(num_boun(1),1)));
+    phi_pat = phi_pat *(phi_pat >= 0)+(phi_pat +pi)*(phi_pat < 0);
+    phi_pattern =[phi_pattern phi_pat];
+end
+pattern_sys = spline(phi_pattern,linspace(0,pi,101));
+% calculate the data around the boundary
+num_phi_fit =num_phi ; 
+num_theta_fit =100;
+number_fit_data = num_theta_fit *num_phi_fit;
+xmn_test = zeros(1,number_fit_data);
+ymn_test = zeros(1,number_fit_data);
+initial_theta = linspace(720/1000*pi/2, 930/1000*pi/2,num_theta_fit);
+flag    = zeros(1,number_fit_data);
+flag2   = zeros(1,number_fit_data);
+% amp_test_1 = zeros(1,number_fit_data);
+% amp_test_2 = zeros(1,number_fit_data);
+% s_test_1   = zeros(1,number_fit_data);
+% s_test_2   = zeros(1,number_fit_data);
+sum_s_test = zeros(1,number_fit_data);
+zeta_test  = zeros(1,number_fit_data);
+amp_test_m = zeros(1,number_fit_data);
+amp_test_p = zeros(1,number_fit_data);
+% gamma_test = zeros(1,number_fit_data);
+% delta_test = zeros(1,number_fit_data);
+amp_airya_test = zeros(1,number_fit_data);
+
+options = optimoptions('fsolve','Display','none');
+options.Algorithm = 'levenberg-marquardt';
+options.StepTolerance = 1.00e-8;
+for j = linspace(0,num_phi_fit-1,num_phi_fit)
+    phi_fit = j*pi/(num_phi_fit-1);
+    phi_fit_sys = ppval(pattern_sys,phi_fit);
+    for i  = linspace(1,num_theta_fit,num_theta_fit)
+        tx_test = real(tmax_num(phi_fit_sys,initial_theta(i)));
+        radius = ((xSol_num(tx_test,phi_fit_sys,initial_theta(i)) - xmn(1))^2 ...
+            +(ySol_num(tx_test,phi_fit_sys,initial_theta(i)))^2)^(1/2) ;
+        xmn_test(i + j*num_theta_fit) = xmn(1) + radius *cos(phi_fit);
+        ymn_test(i + j*num_theta_fit) = radius*sin(phi_fit);
+        fun2 = @(ang)[real(xDetect(ang(1), ang(2))-xmn_test(i + j*num_theta_fit)) ;
+        real(yDetect(ang(1), ang(2))-ymn_test(i + j*num_theta_fit))];
+        x0 = [phi_fit_sys , initial_theta(i)];
+        x1 = [ phi_fit_sys, pi - initial_theta(i)];
+        [mm1,MML,flag(i + j*num_theta_fit),MMM] = fsolve(fun2,x0, options);
+        [mm2,MML2,flag2(i + j*num_theta_fit),MMM2] = fsolve(fun2,x1,options);
+        tx_test_1 = real(tmax_num(mm1(1),mm1(2)));
+        tx_test_2 = real(tmax_num(mm2(1),mm2(2)));
+    %     variables below can be saved for debuging, Later can just be taken as  intermediate variables
+        amp_test_1 = abs(Jacob(0,mm1(1),mm1(2))/Jacob(tx_test_1,mm1(1),mm1(2)))^(1/2);
+        amp_test_2 = abs(Jacob(0,mm2(1),mm2(2))/Jacob(tx_test_2,mm2(1),mm2(2)))^(1/2);
+        s_test_1   = action(mm1(1),mm1(2));
+        s_test_2   = action(mm2(1), mm2(2));
+        sum_s_test(i + j*num_theta_fit)  = s_test_1 +s_test_2;
+        zeta_test(i + j*num_theta_fit) = (3/4*abs(s_test_1 - s_test_2))^(2/3);
+    %     vaiables below is useful
+    % % for s-wave 
+%         amp_test_m(i + j*num_theta_fit) = amp_test_2 - amp_test_1;
+%         amp_test_p(i + j*num_theta_fit) = amp_test_2 + amp_test_1;
+    % % for p0-wave
+%         amp_test_m(i + j*num_theta_fit) = amp_test_2*cos(mm2(2))-amp_test_1*cos(mm1(2));
+%         amp_test_p(i + j*num_theta_fit) = amp_test_2*cos(mm2(2))+amp_test_1*cos(mm1(2));
+    % % for p1-wave(+-)
+        amp_test_m(i + j*num_theta_fit) = amp_test_2*sin(mm2(2))*exp(-1i*mm2(1))-amp_test_1*sin(mm1(2))*exp(-1i*mm1(1));
+        amp_test_p(i + j*num_theta_fit) = amp_test_2*sin(mm2(2))*exp(-1i*mm2(1))+amp_test_1*sin(mm1(2))*exp(-1i*mm1(1));
+   
+        gamma_test = sqrt(pi)*exp(1i*sum_s_test(i + j*num_theta_fit))*amp_test_p(i + j*num_theta_fit)*zeta_test(i + j*num_theta_fit)^(1/4);
+        delta_test = sqrt(pi)*exp(1i*sum_s_test(i + j*num_theta_fit))*amp_test_m(i + j*num_theta_fit)*zeta_test(i + j*num_theta_fit)^(-1/4);
+        amp_airya_test(i + j*num_theta_fit) = abs(gamma_test*airy(-zeta_test(i + j*num_theta_fit))-1i*delta_test*airy(1,-zeta_test(i + j*num_theta_fit)))^2;
+    end
+end
+warning('off','all')
+% the connectiong between $\phi_{sys}$ and $\phi_{fringe}$
+amp_fit_boundary =[];
+x_fit_boundary =[];
+y_fit_boundary =[];
+for i = linspace(0,num_phi_fit-1,num_phi_fit)
+    fit_test_p = amp_test_p(1+i*num_theta_fit:(i+1)*num_theta_fit).*zeta_test(1+i*num_theta_fit:(i+1)*num_theta_fit).^(1/4);  % 
+    fit_test_p_real = real(fit_test_p);
+    fit_test_p_imag = imag(fit_test_p);
+    fit_test_m = amp_test_m(1+i*num_theta_fit:(i+1)*num_theta_fit).*zeta_test(1+i*num_theta_fit:(i+1)*num_theta_fit).^(-1/4);
+    fit_test_m_real = real(fit_test_m);
+    fit_test_m_imag = imag(fit_test_m);
+    radius_test = ((xmn_test(1+i*num_theta_fit:(i+1)*num_theta_fit)-xmn(1)*ones(1,num_theta_fit)).^2*L_au^2+ymn_test(1+i*num_theta_fit:(i+1)*num_theta_fit).^2*L_au^2).^(1/2);
+    [fit_test_p1_real, radius_test11] = prepareCurveData(fit_test_p_real,radius_test);
+    [fit_test_p1_imag, radius_test12] = prepareCurveData(fit_test_p_imag,radius_test);
+   
+    [fit_test_m1_real ,radius_test21] = prepareCurveData(fit_test_m_real,radius_test);
+    [fit_test_m1_imag ,radius_test22] = prepareCurveData(fit_test_m_imag,radius_test); 
+    
+    [sum_s_test1 , radius_test3] = prepareCurveData(sum_s_test(1+i*num_theta_fit:(i+1)*num_theta_fit),radius_test);
+    [zeta_test1 ,radius_test4] = prepareCurveData(zeta_test(1+i*num_theta_fit:(i+1)*num_theta_fit),radius_test);
+    [f_test_p_real ,gof_test_p_r] = fit(radius_test11,fit_test_p1_real,'poly2') ;
+    [f_test_p_imag ,gof_test_p_i] = fit(radius_test12,fit_test_p1_imag,'poly2') ;
+    f_test_p = @(x) f_test_p_real(x)+1i*f_test_p_imag(x);
+%     figure();
+%     plot(f_test_p,radius_test,fit_test_p)
+%     hold on;
+%     plot(radius_exp,f_test_p(radius_exp))
+%     hold off;
+    [f_test_m_real ,gof_test_m_r] = fit(radius_test21,fit_test_m1_real,'poly2');
+    [f_test_m_imag ,gof_test_m_i] = fit(radius_test22,fit_test_m1_imag,'poly2');
+    f_test_m = @(x) f_test_m_real(x) + 1i*f_test_m_imag(x);
+    
+%     figure();
+%     hold on;
+%     plot(f_test_m,radius_test,fit_test_m)
+%     plot(radius_exp,f_test_m(radius_exp));
+%     hold off;
+    [fs_test_p , gof_test_sp] = fit(radius_test3,sum_s_test1,'poly1');
+    [fs_test_m , gof_test_sm] = fit(radius_test4,zeta_test1,'poly1');
+%     radius_exp = linspace(radius_test(1),3*(radius_test(end) - radius_test(1))+radius_test(end),300);
+    radius_exp = linspace(radius_test(1),1.4*10^-3,200);
+    x_fit = radius_exp*cos(i*pi/(num_phi_fit-1))+xmn(1)*L_au;
+    y_fit = radius_exp*sin(i*pi/(num_phi_fit-1));
+    x_fit_boundary = [x_fit_boundary x_fit];
+    y_fit_boundary = [y_fit_boundary y_fit];
+    amp_test_fit = abs(sqrt(pi)*exp(1i*fs_test_p(radius_exp)).*f_test_p(radius_exp) ...
+    .*airy(-fs_test_m(radius_exp))-1i*sqrt(pi)*exp(1i*fs_test_p(radius_exp)).*f_test_m(radius_exp) ...
+    .*airy(1,-fs_test_m(radius_exp))).^2;
+    amp_fit_boundary = [amp_fit_boundary amp_test_fit'];
+end
+T_cpu_2 = cputime;
+% display(T_cpu_2 - T_cpu_1);
+warning('on','all')
+%% Last part save the data and plot out the results and save the datas
+% save fit2d_p1_B11.mat
+x_plot = [xmn*L_au x_fit_boundary];
+y_plot = [ymn*L_au y_fit_boundary];
+amp_plot = [amp_airya amp_fit_boundary];
+x_plot_f = [x_plot x_plot];
+y_plot_f = [y_plot -y_plot];
+amp_plot_f = [amp_plot amp_plot];
+figure();
+% scatter(x_plot,y_plot,3,amp_plot,'filled')
+scatter(x_plot_f,y_plot_f,3,amp_plot_f,'filled')
+mapa = gray;
+mapb = flipud(mapa);
+colormap(mapb);
+box on;
+axis equal;
+normalization_amp = normalization(x_plot_f,y_plot_f,amp_plot_f);
+%save B13_p0.mat y_plot_f x_plot_f amp_plot_f normalization_amp
